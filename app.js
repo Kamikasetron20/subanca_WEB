40060784ab319c58981754068aeb2dc8820f72d8
Kamikasetron20 <santi.pernett15@gmail.com>
2026-05-13 22:42:55 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1035,6 +1035,8 @@ renderMyProps();
     document.getElementById('img-preview').innerHTML = '';
 }
 
+await loadProperties();
+
 editingPropertyId = null;
 
 const btn = document.getElementById('publish-btn');
